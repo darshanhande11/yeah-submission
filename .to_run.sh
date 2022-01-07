ccc62bb76295a95d7d446b405ea985d9a01a0d65
nlok5923 <nlokhande5923@gmail.com>
2022-01-07 15:40:12 +0530

    update sh

---
+++ .to_run.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+# echo "This script is about to run another script."
+DATE_BIN=$(command -v date)
+# dateVariable = `${DATE_BIN} +"%Y-%m-%d %T"`
+#                     date
+dateVariable="2022-01-01 00:00:00"
+# echo "datevariable is" $dateVariable
+counter=1
+var=$(git log 2>&1)
+# echo "this is log" $var
+
+# for i in ${!var[@]}; do
+#   echo "element $i is ${var[$i]}"
+# done
+day=01
+hr=00
+min=$(( $RANDOM % 60 ))
+sec=$(( $RANDOM % 60 ))
+arrayvar=($var)
+#  b5f0886be081d32f887b3dbd76ccab49a2760534
+# for (( i=${#arrayvar[@]}-1 ; i>=0 ; i-- )) ; do
+for i in ${!arrayvar[@]}; do
+ if [ ${#arrayvar[$i]} -eq 40 ];
+ then
+    min=$(( $RANDOM % 60 ))
+    sec=$(( $RANDOM % 60 ))
+    currentDate="2022-01-${day} ${hr}:${min}:${sec}"
+    echo "this is commit ${arrayvar[$i]}"
+    sh ./git-cdc.sh ${arrayvar[$i]} ${currentDate}
+    echo "this is date------------------------------------------------------" ${currentDate}
+    hr=$(( $hr + 1 ))
+    if [ $hr -ge 23 ]; 
+    then
+      hr=00
+      day=$(( $day + 1 ))
+    fi
+ fi 
+done
+# IFS='commit' #setting space as delimiter  
+# read -ra ADDR <<<"$var" #reading str as an array as tokens separated by IFS  
+  
+# for i in "${ADDR[@]}"; #accessing each element of array  
+# do  
+# echo "$i"  
+# done  
+
+# while [ $counter -le 46 ]
+# do
+#     NEWDATE=`TZ=GMT+${counter} date +%Y-%m-%d" "%H:%M:%S`
+#     echo $NEWDATE
+#     ((counter++))
+# done
+# sh ./git-cdc.sh e18f29104c4df5c77f0c7271f68f7b483259c2b2 ${dateVariable}
+# echo "This script has just run another script."
---
+++ git-cdc.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# commit
+# date YYYY-mm-dd HH:MM:SS
+
+commit="$1" datecal="$2" localTime="$3"
+temp_branch="temp-rebasing-branch"
+current_branch="$(git rev-parse --abbrev-ref HEAD)"
+echo "this is local time" $localTime
+# date
+date_timestamp=$(date -d "$datecal $localTime" +%s)
+date_r=$(date -R -d "$datecal $localTime")
+
+if [[ -z "$commit" ]]; then
+    exit 0
+fi
+
+# git checkout -b "$temp_branch" "$commit"
+# GIT_COMMITTER_DATE="$date_timestamp" GIT_AUTHOR_DATE="$date_timestamp" git commit --amend --no-edit --date "$date_r"
+# git checkout "$current_branch"
+# git rebase  --autostash --committer-date-is-author-date "$commit" --onto "$temp_branch"
+# git branch -d "$temp_branch"
+
+git filter-branch --env-filter \
+    `if [ $GIT_COMMIT = $commit ]
+     then
+         export GIT_AUTHOR_DATE="Fri Jan 2 21:38:53 2009 -0800"
+         export GIT_COMMITTER_DATE="$date_timestamp"
+     fi`
